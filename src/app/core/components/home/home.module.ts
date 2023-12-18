01a972b5d06d562fdf251710586138d9c070cea8
Henrique Fernandez <rickfernandez03@gmail.com>
2023-12-18 20:53:34 -0300

    feat: adding cards component into home module & showing profile card in template

--- src/app/core/components/home/home.module.ts
+++ src/app/core/components/home/home.module.ts
@@ -1,13 +1,15 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { CardsModule } from '../cards/cards.module';
 
 @NgModule({
   declarations: [
     HomeComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    CardsModule
   ],
   exports: [
     HomeComponent
