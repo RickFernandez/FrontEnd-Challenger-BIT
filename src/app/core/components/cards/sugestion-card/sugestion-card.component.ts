ed76ff9b726a288d27609a675cdc7681e9c2d2da
Henrique Fernandez <rickfernandez03@gmail.com>
2024-01-10 17:25:27 -0300

    feat: adding suggestion-card

--- src/app/core/components/cards/sugestion-card/sugestion-card.component.ts
+++ src/app/core/components/cards/sugestion-card/sugestion-card.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { UserService } from 'src/app/shared/service/user.service';
+import { Component, Input } from '@angular/core';
+import { UserRes } from 'src/app/shared/model/UserRes';
 
 @Component({
   selector: 'bit-sugestion-card',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./sugestion-card.component.scss']
 })
 export class SugestionCardComponent {
+  @Input() user!: UserRes;
 
+  constructor(private userService: UserService) {}
+
+  onFollow(user: UserRes) {
+    this.userService.followUser(user);
+  }
+
+  onUnfollow(user: UserRes) {
+    this.userService.unfollowUser(user);
+  }
 }
