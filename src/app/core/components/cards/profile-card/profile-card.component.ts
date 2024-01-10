d4d8b4eb575475182fff98a9cbb73a7c2db9ed96
Henrique Fernandez <rickfernandez03@gmail.com>
2024-01-10 17:25:15 -0300

    feat: adding profile-card

--- src/app/core/components/cards/profile-card/profile-card.component.ts
+++ src/app/core/components/cards/profile-card/profile-card.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UserRes } from 'src/app/shared/model/UserRes';
+import { UserService } from 'src/app/shared/service/user.service';
 
 @Component({
   selector: 'bit-profile-card',
@@ -6,5 +8,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile-card.component.scss']
 })
 export class ProfileCardComponent {
+  user!: UserRes;
 
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.loadInitialUser();
+    this.updateUserThumbnail();
+  }
+
+  onFollow(user: UserRes) {
+    this.userService.followUser(user);
+  }
+
+  onUnfollow(user: UserRes) {
+    this.userService.unfollowUser(user);
+  }
+
+  loadInitialUser() {
+    this.user = this.userService.user;
+  }
+
+  updateUserThumbnail() {
+    let thumbnailDiv: HTMLElement = document.getElementById('thumbnail') as HTMLElement;
+    if(thumbnailDiv != null) {
+      thumbnailDiv.style.backgroundImage = `url(${this.user.picture.large})`;
+    }
+  }
+
+  searchUser() {
+    setTimeout(() => {
+      this.getUser();
+    }, 500)
+  }
+
+  getUser(): void {
+    this.userService.getUser().subscribe(
+      (responseApi: UserRes) => { 
+        this.user = responseApi
+        this.updateUserThumbnail();
+       }
+    )
+  }
 }
