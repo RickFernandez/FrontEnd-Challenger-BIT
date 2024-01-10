44d2481f06cb5804e0011e31700dc0a2ef50999f
Henrique Fernandez <rickfernandez03@gmail.com>
2024-01-10 17:25:56 -0300

    feat: adding people-followed-card

--- src/app/core/components/header/people-followed-card/people-followed-card.component.ts
+++ src/app/core/components/header/people-followed-card/people-followed-card.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { UserRes } from 'src/app/shared/model/UserRes';
+import { UserService } from 'src/app/shared/service/user.service';
 
 @Component({
   selector: 'bit-people-followed-card',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./people-followed-card.component.scss']
 })
 export class PeopleFollowedCardComponent {
+  peopleFollowed: UserRes[] = [];
 
+  constructor(private userService: UserService, private cdr: ChangeDetectorRef) {}
+
+  async ngOnInit(): Promise<void> {
+    await this.userService.loadFollowedUsers();
+    this.userService.peopleFollowed.subscribe((followedUsers) => {
+      this.peopleFollowed = followedUsers;
+      this.cdr.detectChanges();
+    });
+  }
+
+  onUnfollow(user: UserRes) {
+    this.userService.unfollowUser(user);
+  }
 }
